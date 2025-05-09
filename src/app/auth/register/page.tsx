07f3234ca958a6574a29f553c92aafbb7a2c71f9
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2025-05-09 18:14:20 +0100

    serbi

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/RegisterForm";
+
+export default function Register() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[url('https://expertcarreview.co.uk/wp-content/uploads/2023/02/Good-Garage-Service.jpg')] bg-cover">
+      <RegisterForm />
+    </div>
+  );
+}
---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,62 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Car, Home, Newspaper } from "lucide-react";
+
+const items = [
+  {
+    title: "Home",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Garage",
+    url: "/dashboard/garage",
+    icon: Car,
+  },
+  {
+    title: "Reports",
+    url: "/dashboard/reports",
+    icon: Newspaper,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="border-none">
+      <SidebarContent className=" bg-background text-foreground border-r">
+        <SidebarHeader>
+          <div className=" text-4xl font-bold mt-8 flex items-center gap-2">
+            <div>SmartAuto</div>
+            <div>
+              <Car size={24} />
+            </div>
+          </div>
+        </SidebarHeader>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
